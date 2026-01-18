3dcb8c61f94d4ded389ba41dbc99ead46a26e585
Justin Knowles <jknowles370@gmail.com>
2026-01-18 10:06:34 -0600

    cleaned simulation

--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ import argparse
 import pandas as pd
 import random
 from tabulate import tabulate
+from typing import Any
 
 from cards import card_int, suit_int, SUITS
 from game import EuchreGame
@@ -67,22 +68,22 @@ class SimulationStats:
 
 
 def simulate_hand(
-    fixed_hand: list[int],
-    fixed_upcard: int,
-    fixed_seat: int,
-    force_suit_choice: int = None,
-    force_alone_choice: bool = False,
+    hand: list[int],
+    upcard: int,
+    seat: int,
+    suit: int = None,
+    alone: bool = False,
     strategies: list[Strategy] = None,
     split_by_maker: bool = False,
     trials: int = 50_000,
-    rng_seed: int = None,
+    seed: int = None,
     verbose: bool = False,
 ):
     """
-    fixed_seat of 0 is dealer
+    seat of 0 is dealer
     """
     stats = SimulationStats(split_by_maker=split_by_maker)
-    rng = random.Random(rng_seed)
+    rng = random.Random(seed)
 
     if strategies is None:
         strategies = [SimpleStrategy() for _ in range(4)]
@@ -90,18 +91,48 @@ def simulate_hand(
     for i in range(trials):
         game = EuchreGame(strategies=strategies, verbose=verbose)
 
-        outcome = game.play_hand(
-            True,
-            fixed_hand,
-            fixed_upcard,
-            fixed_seat,
-            force_suit_choice,
-            force_alone_choice,
-            rng,
-        )
+        outcome = game.play_hand(True, hand, upcard, seat, suit, alone, rng)
         stats.record(outcome)
 
     return stats.report()
+
+
+def simulate_choices(
+    hand: list[int],
+    upcard: int,
+    seat: int,
+    strategies: list[Strategy] = None,
+    split_by_maker: bool = False,
+    trials: int = 50_000,
+    seed: int = None,
+    verbose: bool = False,
+):
+    suits = [0, 1, 2, 3, -1]  # All suits and pass
+    alones = [False, True]
+    results = []
+    for suit in suits:
+        suit_str = SUITS[suit] if suit != -1 else "Pass"
+        for alone in alones:
+            # Dealer can't pass in the second round, so no reason to simulate this
+            if seat == 0 and suit == -1:
+                continue
+            result = simulate_hand(
+                hand,
+                upcard,
+                seat,
+                suit,
+                alone,
+                strategies,
+                split_by_maker,
+                trials,
+                seed,
+                verbose,
+            )
+            base = {"suit_choice": suit_str, "alone_choice": alone}
+            rows = result if isinstance(result, list) else [result]
+            for row in rows:
+                results.append(base | row)
+    return results
 
 
 # Example CLI
@@ -117,12 +148,12 @@ if __name__ == "__main__":
     parser.add_argument("--seed", type=int, default=42)
     args = parser.parse_args()
 
-    # hand = ["Jc", "Js", "Jh", "Jd", "9d"]
-    # upcard = "9h"
-    # hand = ["9c", "Tc", "Jc", "Qc", "Kc"]
-    # upcard = "Ac"
-    hand = ["Jc", "Ac", "9c", "Ah", "Qd"]
-    upcard = "9h"
+    # hand_str = ["Jc", "Js", "Jh", "Jd", "9d"]
+    # upcard_str = "9h"
+    # hand_str = ["9c", "Tc", "Jc", "Qc", "Kc"]
+    # upcard_str = "Ac"
+    hand_str = ["Jc", "Ac", "9c", "Ah", "Qd"]
+    upcard_str = "9h"
     seat = 0
     # seat = 1
     # seat = 2
@@ -133,13 +164,12 @@ if __name__ == "__main__":
     # others_strats = [PassiveStrategy() for _ in range(3)]
     others_strats = [SimpleStrategy() for _ in range(3)]
 
-    hand_int = card_int(hand)
-    upcard_int = card_int(upcard)
+    hand = card_int(hand_str)
+    upcard = card_int(upcard_str)
 
     strategies = others_strats.copy()
     strategies.insert(seat, my_strat)
 
-    # print("Simulate passing always")
     """
     force_suit_name = None
     # force_suit_name = "clubs"
@@ -152,8 +182,8 @@ if __name__ == "__main__":
     # force_alone_choice = True
     force_suit_choice = suit_int(force_suit_name)
     report_pass = simulate_hand(
-        hand_int,
-        upcard_int,
+        hand,
+        upcard,
         seat,
         args.trials,
         force_suit_choice,
@@ -172,31 +202,16 @@ if __name__ == "__main__":
     force_suit_choices = suit_int(force_suit_names)
 
     # Collect results for all combinations
-    results = []
-    for force_suit_choice in force_suit_choices:
-        suit_choice_str = (
-            SUITS[force_suit_choice] if force_suit_choice != -1 else "Pass"
-        )
-        for force_alone_choice in force_alone_choices:
-            # Dealer can't always pass, so no reason to simulate this
-            if seat == 0 and force_suit_choice == -1:
-                continue
-            result = simulate_hand(
-                hand_int,
-                upcard_int,
-                seat,
-                force_suit_choice,
-                force_alone_choice,
-                strategies,
-                args.split_maker,
-                args.trials,
-                args.seed,
-                args.verbose,
-            )
-            base = {"suit_choice": suit_choice_str, "alone_choice": force_alone_choice}
-            rows = result if isinstance(result, list) else [result]
-            for row in rows:
-                results.append(base | row)
+    results = simulate_choices(
+        hand,
+        upcard,
+        seat,
+        strategies,
+        args.split_maker,
+        args.trials,
+        args.seed,
+        args.verbose,
+    )
 
     # Print results as a table
     # print(tabulate(results, headers="keys", tablefmt="grid"))
